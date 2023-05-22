c322b69a4f7f2a7a022d65548902fc1fa8fd417f
f22linhe <f22linhe@student.his.se>
2023-05-22 22:47:54 +0200

    lade till kod i SecondActivity för att kunna visa en Internal Web Page samt skapade en tillhörande layout med en WebView att visa Internal Web Page i.

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -1,6 +1,35 @@
 package com.example.project;
 
-import android.app.Activity;
 
-public class SecondActivity extends Activity {
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class SecondActivity extends AppCompatActivity {
+
+    WebView myWebView;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        myWebView = findViewById(R.id.aboutWebView);
+        myWebView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        myWebView.loadUrl("file:///android_asset/about.html");
+
+    }
 }
+
+
