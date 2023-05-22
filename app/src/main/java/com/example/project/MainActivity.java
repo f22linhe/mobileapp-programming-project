8ff1ccfb8df15fbb3c456ab04990241130889e8c
f22linhe <f22linhe@student.his.se>
2023-05-22 22:19:32 +0200

    Hämtar JSON data från URL. Läser in och loggar data i logg.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.project;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
+
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=f22linhe";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +16,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        new JsonTask(this).execute(JSON_URL);
     }
 
+    @Override
+    public void onPostExecute(String json) {
+        Log.d("Vulkaner", json);
+
+    }
 }
